c9cd92ddaa58bb443b1b0e25d29a938d5ca9380c
rivlev <proflevitan@gmail.com>
2019-11-12 00:30:53 -0500

    generic

--- iterators.cpp
+++ iterators.cpp
@@ -2,91 +2,74 @@
 #include <iostream>
 #include <vector>
 
-template <class T>
-void selectionSort(std::vector<T>& v) {
+
+template <class Iter>
+void selectionSort(Iter begin, Iter end) {
 	/*******Your code here********/
 
 	/*****************************/
 	/***Test code: do not modify***/
-	std::vector<T> vv(v);
-	std::sort(vv.begin(), vv.end());
-	auto it1 = v.begin(), it2 = vv.begin();
-	while (it1 != v.end() && it2 != vv.end()) {
-		if (*it1 != *it2) throw("List not sorted\n");
+	typedef typename std::iterator_traits<Iter>::value_type value_type;
+	std::vector<value_type> vv(begin, end);
+	std::sort(begin, end);
+	auto it1 = begin, it2 = vv.begin();
+	while (it1 != end  && it2 != vv.end()) {
+		if (*it1 != *it2) throw("Range not sorted\n");
 		++it1; ++it2;
 	}
 	/*****************************/
 }
 
-template <class T>
-void reverseList(std::vector<T>& v) {
+template <class Iter>
+void reverseRange(Iter begin, Iter end) {
 	/***Test code: do not modify***/
-	std::vector<T> vv(v);
+	typedef typename std::iterator_traits<Iter>::value_type value_type;
+	std::vector<value_type> vv(begin, end);
 	/*****************************/
 	/*******Your code here********/
 
 	/*****************************/
 	/***Test code: do not modify***/
-	auto it1 = v.begin(), it2 = vv.end();
+	auto it1 = begin, it2 = vv.end();
 	--it2;
-	while (it1 != v.end()) {
-		if (*it1 != *it2) throw("List not reversed\n");
+	while (it1 != end) {
+		if (*it1 != *it2) throw("Range not reversed\n");
 		++it1; --it2;
 	}
 	/*****************************/
 }
 
-template <class T>
-void removeEveryOtherELement(std::vector<T>& v) {
-	/***Test code: do not modify***/
-	std::vector<T> vv(v);
-	/*****************************/
+template <class Iter>
+void printRange(Iter begin, Iter end) {
 	/*******Your code here********/
-	// Remove every *odd* element (at index 1, 3, ...)
 
 	/*****************************/
-	/***Test code: do not modify***/
-	auto it1 = v.begin(), it2 = vv.begin();
-	while (it1 != v.end() && it2 != vv.end()) {
-		if (*it1 != *it2) throw("Even indices not equal\n");
-		++it1; ++it2;
-		if (it1 == v.end()) break;
-		if (*it1 == *it2) throw("Odd index not removed\n");
-		++it1;
-	}
-	/*****************************/
-}
-
-void printList(const std::vector<int>& v) {
-	for  (int x : v) std::cout << x << " ";
-	std::cout << std::endl;
-}
-
-void fillList(std::vector<int>& v) {
-	for (int i = 0; i < 23; i++) v.push_back(i);
-	std::random_shuffle(v.begin(), v.end());
 }
 
 int main() {
 
 	std::vector<int> v;
-	fillList(v);
+	for (int i = 0; i < 23; i++) v.push_back(i);
+	std::random_shuffle(v.begin(), v.end());
+
+	bool errored = false;
 	
 	try {
-		selectionSort(v);
+		selectionSort(v.begin(), v.end());
 	} catch (char const* err) {
 		std::cout << err << std::endl;
+		errored = true;
 	}
 
-	try {
-		removeEveryOtherELement(v);
-	} catch (char const* err) {
-		std::cout << err << std::endl;
-	}
 
 	try {
-		reverseList(v);
+		reverseRange(v.begin(), v.end());
 	} catch (char const* err) {
 		std::cout << err << std::endl;
+		errored = true;
 	}
+
+	printRange(v.begin(), v.end());
+
+	if (errored) exit(EXIT_FAILURE);
 }
